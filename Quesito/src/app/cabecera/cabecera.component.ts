993a044319311167977e53f1e7ec2fd15ab59b7c
oskkrx <oskr_16_94@live.com.ar>
2022-02-23 23:30:30 -0300

    Primera subida

---
+++ Quesito/src/app/cabecera/cabecera.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.css']
+})
+export class CabeceraComponent implements OnInit {
+
+  title = 'Task List';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
